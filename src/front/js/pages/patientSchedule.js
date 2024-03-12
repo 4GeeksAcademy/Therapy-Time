cc33f0666a0539b795b72665fcab3feb5b3d9c7d
Federico Resano <123256603+fede1525@users.noreply.github.com>
2024-03-12 22:52:26 +0000

    commit

--- src/front/js/pages/patientSchedule.js
+++ src/front/js/pages/patientSchedule.js
@@ -1,14 +1,49 @@
 import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
+import Calendar from "react-calendar";
 
 export const PatientSchedule = () => {
+    const { actions } = useContext(Context)
+    const [date, setDate] = useState(new Date());
+    const [showModal, setShowModal] = useState(false);
+
+    const dateChange = (newDate) => {
+        setDate(newDate);
+    };
+
+    const toggleModal = () => {
+        setShowModal(!showModal)
+    }
+
+    const renderModal = () => {
+
+    }
 
 
 
     return (
         <div className="container">
-            <h1>Por hacer: pagina de calendario para paciente</h1>
+            <div className="row">
+                <div className="col-6">
+                    <Calendar
+                        onChange={dateChange}
+                        value={date}
+                    />
+                </div>
+                <div className="col-6" style={{ backgroundColor: '#Fafafa' }}>
+                    <h3>Tu proximo turno: {nextTurn}</h3>
+                    <p>Acceso a la sala virtual: </p>
+                    <a></a>
+                    <div className="col">
+                        <button>Cancelar turno</button>
+                    </div>
+                    <div className="col">
+                        <button>Modificar turno</button>
+                    </div>
+                    <button>Abonar por Mercado Pago</button>
+                </div>
+            </div>
         </div>
     )
 }
