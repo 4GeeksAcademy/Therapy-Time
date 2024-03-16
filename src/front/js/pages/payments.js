9b4e767439c0ecce7f88df435548915afc5449ec
Federico Resano <123256603+fede1525@users.noreply.github.com>
2024-03-16 01:38:06 +0000

    Arreglos en la pagina de pagos

--- src/front/js/pages/payments.js
+++ src/front/js/pages/payments.js
@@ -7,59 +7,63 @@ export const Payments = () => {
     const { store, actions } = useContext(Context);
 
 
-
     return (
         <div>
             <NavbarPatient />
-            <div className="container bg-light" >
-                <div className="row">
-                    <div className="col">
+            <div className="row">
+                <div className="col-4 p-4">
+                    <div className="row" style={{ marginTop: '80px' }}>
                         <h3>Mis pagos</h3>
+                        <label>Medio de pago</label>
                         <input
-                            label="Medio de pago"
+                            type="text"
                         />
+                    </div>
+                    <div className="row">
+                        <label>Desde</label>
                         <input
-                            label="Desde:"
                             type="date"
                         />
+                    </div>
+                    <div className="row">
+                        <label>Hasta:</label>
                         <input
-                            label="Hasta:"
                             type="date"
                         />
-                        <button>Filtrar</button>
                     </div>
-                    <div className="col">
-                        <table className="table table-striped table-bordered" style={{ borderColor: '#C4C4C4' }}>
-                            <thead style={{ backgroundColor: '#798694' }}>
-                                <tr>
-                                    <th scope="col">Fecha de pago</th>
-                                    <th scope="col">Medio de pago</th>
-                                    <th scope="col">Monto</th>
-                                    <th scope="col">Concepto</th>
-                                    <th scope="col">Comprobante</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td colspan="2">Larry the Bird</td>
-                                    <td>@twitter</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                    <button>Filtrar</button>
+                </div>
+                <div className="col-8">
+                    <table className="table table-striped table-bordered" style={{ borderColor: '#C4C4C4', marginTop: '120px' }}>
+                        <thead style={{ backgroundColor: '#929DAB' }}>
+                            <tr style={{ color: 'white' }}>
+                                <th scope="col">Fecha de pago</th>
+                                <th scope="col">Medio de pago</th>
+                                <th scope="col">Monto</th>
+                                <th scope="col">Concepto</th>
+                                <th scope="col">Comprobante</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td colspan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
